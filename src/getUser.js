60f5b640c4fe94e18ac74f0d05b06970d13275bd
safar1212 <mm9240584@gmail.com>
2022-06-16 22:55:50 +0500

    Resolve linters error

--- src/getUser.js
+++ src/getUser.js
@@ -1,29 +1,32 @@
-import { url } from "./submit";
-
-
 const getUserInput = async (url) => {
-    let user;
-    let data;
-    try {
-      const response = await fetch(url);
-      data = await response.json();
-      user = data.result;
-    } catch (e) {
-      console.log(`Error: ${e}`);
-    }
+  let user;
+  let data;
+  try {
+    const response = await fetch(url);
+    data = await response.json();
+    user = data.result;
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
 
-    const listContainer = document.querySelector('#list-container');
-  
-    while (listContainer.hasChildNodes()) {
-      listContainer.removeChild(listContainer.firstChild);
-    }
-  
-    for (let i = 0; i < user.length; i += 1) {
-      const list = document.createElement('li');
-      const text = document.createTextNode(`${user[i].user}: ${user[i].score}`);
-      list.appendChild(text);
-      listContainer.appendChild(list);
-    }
-}
+  const listContainer = document.querySelector('#list-container');
+  listContainer.innerHTML = '';
+
+  // while (listContainer.hasChildNodes()) {
+  //   listContainer.removeChild(listContainer.firstChild);
+  // }
+  let list = '';
+  user.forEach((person) => {
+    list += `<li>${person.user}: ${person.score}</li>`;
+  });
+  listContainer.innerHTML = list;
+
+  // for (let i = 0; i < user.length; i += 1) {
+  //   const list = document.createElement('li');
+  //   const text = document.createTextNode(`${user[i].user}: ${user[i].score}`);
+  //   list.appendChild(text);
+  //   listContainer.appendChild(list);
+  // }
+};
 
 export default getUserInput;
