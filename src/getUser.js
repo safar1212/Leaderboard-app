a9038315161e00005b9b0bd053f8296eda05fad1
Safar Ali <miqra019@gmail.com>
2022-06-21 20:07:26 +0500

    Update getUser.js

--- src/getUser.js
+++ src/getUser.js
@@ -11,22 +11,13 @@ const getUserInput = async (url) => {
 
   const listContainer = document.querySelector('#list-container');
   listContainer.innerHTML = '';
-
-  // while (listContainer.hasChildNodes()) {
-  //   listContainer.removeChild(listContainer.firstChild);
-  // }
+  
   let list = '';
   user.forEach((person) => {
     list += `<li>${person.user}: ${person.score}</li>`;
   });
   listContainer.innerHTML = list;
 
-  // for (let i = 0; i < user.length; i += 1) {
-  //   const list = document.createElement('li');
-  //   const text = document.createTextNode(`${user[i].user}: ${user[i].score}`);
-  //   list.appendChild(text);
-  //   listContainer.appendChild(list);
-  // }
 };
 
 export default getUserInput;
